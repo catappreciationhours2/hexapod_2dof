e63a462b634d67f2dc06619770b25cb900359fea
Noella Horo <nhoro@andrew.cmu.edu>
2026-03-10 06:16:25 -0400

    Fix: Remove deprecated pytest-runner to resolve ImpImporter error

--- Code/hexapod/setup.py
+++ Code/hexapod/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+
 setup(
     name='hexapod',
     packages=find_packages(include=['hexapod']),
@@ -8,10 +9,18 @@ setup(
     author='Nabeel Chowdhury',
     author_email='nabeel.chowdhury@case.edu',
     license='GPL-2.0',
-    install_requires=['numpy>=1.22.3', 'pyserial>=3.5',
-                      'adafruit-circuitpython-mcp3xxx>=1.4.7', 'inputs>=0.5'],
-    setup_requires=['pytest-runner>=6.0.0'],
-    tests_require=['pytest>=7.1.0', 'numpy>=1.22.3', 'pyserial>=3.5',
-                   'adafruit-circuitpython-mcp3xxx>=1.4.7'],
+    install_requires=[
+        'numpy>=1.22.3', 
+        'pyserial>=3.5',
+        'adafruit-circuitpython-mcp3xxx>=1.4.7', 
+        'inputs>=0.5'
+    ],
+    # REMOVED: setup_requires=['pytest-runner>=6.0.0'],
+    tests_require=[
+        'pytest>=7.1.0', 
+        'numpy>=1.22.3', 
+        'pyserial>=3.5',
+        'adafruit-circuitpython-mcp3xxx>=1.4.7'
+    ],
     test_suite='tests',
 )
